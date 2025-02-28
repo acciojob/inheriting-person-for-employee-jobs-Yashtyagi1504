a530934e7a27eb253742d087f4f1e101ffff9402
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-03-01 03:25:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ person.greet();
 // Expected Output: Hello, my name is Alice, I am 25 years old.
 
 // Creating an instance of Employee
-const employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet();
+// const employee = new Employee("Bob", 30, "Manager");
+// employee.jobGreet();
 // Expected Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
 
 // Do not change code below this line
